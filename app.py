b8e6c97c02b7356571f841043946f96d672103c0
tonic <tonicbupt@gmail.com>
2014-09-18 17:37:58 +0800

    use it this way

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 # coding: utf-8
 
 import os
-import sys
 import yaml
-from pkg_resources import load_entry_point
+from docker_registry.wsgi import application
 
 
-with open('config.yaml') as f:
-    os.environ.update(yaml.load(f))
-    if 'DOCKER_REGISTRY_CONFIG' in os.environ:
-        os.environ['DOCKER_REGISTRY_CONFIG'] = os.path.abspath(os.environ['DOCKER_REGISTRY_CONFIG'])
+class NBEWrapper(object):
+
+    def __init__(self, app):
+        self.app = app
+        self.load_environ()
+
+    def __call__(self, env, start_response):
+        return self.app(env, start_response)
+
+    def load_environ(self):
+        with open('config.yaml') as f:
+            env = yaml.load(f)
+            for key, value in env.iteritems():
+                if key.isupper():
+                    os.environ[key] = value
+            os.environ['SQLALCHEMY_INDEX_DATABASE'] = 'mysql://{username}:{password}@{host}:{port}/{db}'.format(**env['mysql'])
+            os.environ['DOCKER_REGISTRY_CONFIG'] = os.path.abspath(os.environ['DOCKER_REGISTRY_CONFIG'])
+            os.environ['CACHE_REDIS_HOST'] = str(env['redis']['host'])
+            os.environ['CACHE_REDIS_PORT'] = str(env['redis']['port'])
 
 
-sys.exit(load_entry_point('docker-registry==0.9.0', 'console_scripts', 'docker-registry')())
+app = NBEWrapper(application)
