6e4d26f73d69eb1f1fe0b77a2458ce012db087f8
tonic <tonicbupt@gmail.com>
2014-09-18 18:09:40 +0800

    ok

---
+++ gunicorn_config.py
@@ -0,0 +1,12 @@
+reload = True
+bind = '0.0.0.0:5000'
+graceful_timeout = 3600
+timeout = 3600
+worker_class = 'gevent'
+max_requests = 100
+workers = 4
+log_level = 'info'
+debug = False
+accesslog = '-'
+access_log_format = ('%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" '
+                     '"%(a)s" %(D)s %({X-Docker-Size}o)s')
