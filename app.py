a75037d707c486bdf6dae8dd69b431fbffb82cb8
tonic <tonicbupt@gmail.com>
2014-09-18 17:52:08 +0800

    this way

--- app.py
+++ app.py
@@ -2,6 +2,19 @@
 
 import os
 import yaml
+
+
+with open('config.yaml') as f:
+    env = yaml.load(f)
+    for key, value in env.iteritems():
+        if key.isupper():
+            os.environ[key] = value
+    os.environ['SQLALCHEMY_INDEX_DATABASE'] = 'mysql://{username}:{password}@{host}:{port}/{db}'.format(**env['mysql'])
+    os.environ['DOCKER_REGISTRY_CONFIG'] = os.path.abspath(os.environ['DOCKER_REGISTRY_CONFIG'])
+    os.environ['CACHE_REDIS_HOST'] = str(env['redis']['host'])
+    os.environ['CACHE_REDIS_PORT'] = str(env['redis']['port'])
+
+
 from docker_registry.wsgi import application
 
 
@@ -9,21 +22,9 @@ class NBEWrapper(object):
 
     def __init__(self, app):
         self.app = app
-        self.load_environ()
 
     def __call__(self, env, start_response):
         return self.app(env, start_response)
-
-    def load_environ(self):
-        with open('config.yaml') as f:
-            env = yaml.load(f)
-            for key, value in env.iteritems():
-                if key.isupper():
-                    os.environ[key] = value
-            os.environ['SQLALCHEMY_INDEX_DATABASE'] = 'mysql://{username}:{password}@{host}:{port}/{db}'.format(**env['mysql'])
-            os.environ['DOCKER_REGISTRY_CONFIG'] = os.path.abspath(os.environ['DOCKER_REGISTRY_CONFIG'])
-            os.environ['CACHE_REDIS_HOST'] = str(env['redis']['host'])
-            os.environ['CACHE_REDIS_PORT'] = str(env['redis']['port'])
 
 
 app = NBEWrapper(application)
